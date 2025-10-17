f6f2b7da17c11c3a29d404e5694df7308b4c23f3
TrivmviraT <consvpr@outlook.es>
2025-10-17 22:33:35 +0200

    todo

--- src/models.py
+++ src/models.py
@@ -4,16 +4,28 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
 
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    usuario_id = db.Column(db.Integer, db.ForeignKey('usuario.id'))
+    imagen_url = db.Column(db.String(200))
+    descripcion = db.Column(db.String(200))
+
+
+class Comentario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    usuario_id = db.Column(db.Integer, db.ForeignKey('usuario.id'))
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
+    texto = db.Column(db.String(200))
+
+
+class Seguidor(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    usuario_id = db.Column(db.Integer, db.ForeignKey('usuario.id'))
+    seguido_id = db.Column(db.Integer, db.ForeignKey('usuario.id'))
